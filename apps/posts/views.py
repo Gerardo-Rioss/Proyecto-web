393afb700951521bd529d27b83c2012c8a7799b8
Gerardo-Rioss <gerarios26@gmail.com>
2023-07-31 13:29:25 -0300

    hasta formularios

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def posts(request):
+""" def posts(request):
     posts = Post.objects.all()
-    return render(request, 'posts.html', {'posts': posts})
+    return render(request, 'posts.html', {'posts': posts}) """
+
+
+# Vista basada en clases
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/posts.html"
+    context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post_individual.html"
+    context_object_name = "posts"
+    pk_url_kwarg = 'id'
+    queryset = Post.objects.all()
+
+
+
